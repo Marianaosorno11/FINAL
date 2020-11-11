999bcdb4f0801bb47b826cc85f7959c9e90ce5b4
Marianaosorno11 <70726712+Marianaosorno11@users.noreply.github.com>
2020-11-10 23:18:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,106 @@
+var ac1=function()
+{
+var a1=document.getElementById("a1");
+var s1=document.getElementById("s1");
+var v1=document.getElementById("v1");
+
+s1.autoplay=true;
+s1.load();
+
+if ("s1"=="s1")
+{
+setTimeout(stop2,1000);
+}
+}
+
+var ac2=function()
+{
+var a2=document.getElementById("a2");
+var s2=document.getElementById("s2");
+var v2=document.getElementById("v2");
+
+s2.autoplay=true;
+s2.load();
+
+if ("s2"=="s2")
+{
+setTimeout(stop2,1000);
+}
+}
+
+var ac3=function()
+{
+var a3=document.getElementById("a3");
+var s3=document.getElementById("s3");
+var v3=document.getElementById("v3");
+
+s3.autoplay=true;
+s3.load();
+
+if ("s3"=="s3")
+{
+setTimeout(stop2,1000);
+}
+}
+
+var sp1=function()
+{
+var p1=document.getElementById("p1");
+var m1=document.getElementById("m1");
+var sn1=document.getElementById("sn1");
+
+sn1.autoplay=true;
+sn1.load();
+
+if ("sn1"=="sn1")
+{
+setTimeout(stop2,1000);
+}
+}
+
+var sp2=function()
+{
+var p2=document.getElementById("p2");
+var m2=document.getElementById("m2");
+var sn2=document.getElementById("sn2");
+
+sn2.autoplay=true;
+sn2.load();
+
+if ("sn3"=="sn3")
+{
+setTimeout(stop2,1000);
+}
+}
+
+
+var sp3=function()
+{
+var p3=document.getElementById("p3");
+var m3=document.getElementById("m3");
+var sn3=document.getElementById("sn3");
+
+sn3.autoplay=true;
+sn3.load();
+
+if ("sn3"=="sn3")
+{
+setTimeout(stop2,1000);
+}
+}
+
+
+var sp4=function()
+{
+var p4=document.getElementById("p4");
+var m4=document.getElementById("m4");
+var sn4=document.getElementById("sn4");
+
+sn4.autoplay=true;
+sn4.load();
+
+if ("sn4"=="sn4")
+{
+setTimeout(stop2,1000);
+}
+}
